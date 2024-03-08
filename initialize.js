fec566158569a981e5864c769cebed432371058b
Aleksandr Kukharenko <aleksandr@titan.io>
2024-03-08 15:49:38 +0200

    feat: polling instead of wss events

--- initialize.js
+++ initialize.js
@@ -1,32 +1,107 @@
 const { Web3 } = require("web3");
-const Web3WsProvider = require('web3-providers-ws').default;
+const Web3WsProvider = require("web3-providers-ws").default;
+const { createPublicClient, webSocket, parseAbi } = require("viem");
 
 const { CloneFactory } = require("contracts-js");
 
 const { ContractsLoader } = require("./src/ContractsLoader");
 const { ContractsInMemoryIndexer } = require("./src/ContractsInMemoryIndexer");
 const { ContractMapper } = require("./src/ContractMapper");
-const { ContractEventsListener } = require("./src/ContractEventsListener");
+
+/**
+ *
+ * @param {import('viem').PublicClient} client
+ * @param {any} loader
+ * @param {any} config
+ * @param {(id, blockNumber) => void} onLogUpdate
+ * @returns {Promise<void>}
+ */
+const startWatch = async (client, loader, config, onLogUpdate) => {
+  try {
+    const contractAddresses = await loader.getContractList();
+    const addresses = [config.CLONE_FACTORY_ADDRESS, ...contractAddresses];
+  
+    const cloneFactoryEvents = [
+      "contractCreated",
+      "clonefactoryContractPurchased",
+      "contractDeleteUpdated",
+      "purchaseInfoUpdated",
+    ];
+    const eventsAbi = [
+      // Clone Factory Events
+      "event contractCreated(address indexed _address, string _pubkey)",
+      "event clonefactoryContractPurchased(address indexed _address)",
+      "event contractDeleteUpdated(address _address, bool _isDeleted)",
+      "event purchaseInfoUpdated(address indexed _address)",
+  
+      // Implementation Events
+      "event contractClosed(address indexed _buyer)",
+      "event cipherTextUpdated(string newCipherText)",
+      "event destinationUpdated(string newValidatorURL, string newDestURL)",
+    ];
+  
+    const unwatch = client.watchEvent({
+      address: addresses,
+      events: parseAbi(eventsAbi),
+      poll: true,
+      pollingInterval: 1000,
+      onLogs: (logs) => {
+        logs.forEach((log) => {
+          const { eventName, args, address, blockNumber } = log;
+          let contractAddress = null;
+          if (cloneFactoryEvents.includes(eventName)) {
+            contractAddress = args._address;
+          } else {
+            contractAddress = address;
+          }
+          console.log(`Received log for contract: ${contractAddress}`);
+  
+          onLogUpdate(contractAddress, Number(blockNumber));
+          if (eventName === "contractCreated") {
+            console.log('Got contract created event, restating watch')
+            unwatch();
+            startWatch(client, loader, config, onLogUpdate);
+          }
+        });
+      },
+    });
+    console.log(
+      `Started listen events for contracts: ${JSON.stringify(addresses)}, amount: ${addresses.length}`
+    );
+    return { addresses };
+  } catch (err) {
+    console.error("Error starting watch", err);
+    process.exit(1);
+  }
+};
 
 const initialize = async (config) => {
-  const ws = new Web3WsProvider(config.WS_ETH_NODE_URL,{}, {
-    autoReconnect: true,
-    delay: 1000,
-    maxAttempts: 10,
-  })
+  const client = createPublicClient({
+    transport: webSocket(config.WS_ETH_NODE_URL),
+  });
+
+  const ws = new Web3WsProvider(
+    config.WS_ETH_NODE_URL,
+    {},
+    {
+      autoReconnect: true,
+      delay: 1000,
+      maxAttempts: 10,
+    }
+  );
   const web3 = new Web3(ws);
 
-  ws.on('disconnect', () => {
-    console.log('ws provider disconnected (disconnect event)')
-    process.exit(1)
+  ws.on("disconnect", () => {
+    console.log("ws provider disconnected (disconnect event)");
+    process.exit(1);
   });
-  ws.on('error', (error) => {
-    console.error('ws provider error', error)
+  ws.on("error", (error) => {
+    console.error("ws provider error", error);
   });
-  ws.on('end', (d) => {
-    console.log('ws provider disconnected (end event)')
-    process.exit(1)
-  })
+  ws.on("end", (d) => {
+    console.log("ws provider disconnected (end event)");
+    process.exit(1);
+  });
 
   const cloneFactory = CloneFactory(web3, config.CLONE_FACTORY_ADDRESS);
 
@@ -47,11 +122,6 @@ const initialize = async (config) => {
     }
   };
 
-  const eventsListener = ContractEventsListener.create(
-    cloneFactory,
-    onEventUpdate
-  );
-
   /**
    *
    * @param {string} contractId
@@ -61,8 +131,9 @@ const initialize = async (config) => {
    */
   const onContractLoad = (contractId, contract, implInstance, blockNumber) => {
     indexer.upsert(contractId, contract, blockNumber);
-    eventsListener.listenContract(contractId, implInstance);
   };
+
+  await startWatch(client, loader, config, onEventUpdate);
 
   await loader.loadAll(onContractLoad);
   console.log("All Contracts loaded");
